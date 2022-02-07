a734e693d8b715d5b2c5765e0a75282fc96c4457
katopuebla <katopuebla@gmail.com>
2022-02-07 02:00:57 -0600

    buid prod to docs folder

---
+++ docs/precache-manifest.e8045d94851c8af8190c55884b08aa03.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90943ae4b6083639b2116775a3e13fd6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a12c3e34fdeac5f5ccb",
+    "url": "/static/css/2.81e55c90.chunk.css"
+  },
+  {
+    "revision": "1cc2f3122cef494eb19b",
+    "url": "/static/css/main.686678d1.chunk.css"
+  },
+  {
+    "revision": "1a12c3e34fdeac5f5ccb",
+    "url": "/static/js/2.a0601015.chunk.js"
+  },
+  {
+    "revision": "867443ad4a443db6ed0e2e8f2a12c9e1",
+    "url": "/static/js/2.a0601015.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cc2f3122cef494eb19b",
+    "url": "/static/js/main.1168b630.chunk.js"
+  },
+  {
+    "revision": "a2f4ff47f946d5bb712c",
+    "url": "/static/js/runtime-main.ef0be1d5.js"
+  }
+]);
